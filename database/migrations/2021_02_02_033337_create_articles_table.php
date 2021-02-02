bc48099e23de94087dac4f8fc9577f199da1b19e
krisdianwidya <krisdianwidya@gmail.com>
2021-02-02 10:48:43 +0700

    create Model and migration

---
+++ database/migrations/2021_02_02_033337_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->integer('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->char('title', 100);
+            $table->char('description', 200);
+            $table->text('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
